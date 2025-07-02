d97ed7116eb0151d6c13de9398e6e16e20ab70fe
Bhuwan <bhuwanthapa4747@gmail.com>
2025-07-02 20:26:34 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import SkillChart from "./components/SkillChart";
+import FunFacts from "./components/FunFacts";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-white text-gray-800 dark:bg-gray-900 dark:text-white transition-colors duration-300">
+            <Navbar />
+            <Hero />
+            <Projects />
+            <SkillChart/>
+            <About />
+            <FunFacts />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
-
 export default App;
+
